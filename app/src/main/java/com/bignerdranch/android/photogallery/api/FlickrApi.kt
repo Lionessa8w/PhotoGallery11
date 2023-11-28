cb11fc7a827f6440d69c945b16e4ad19396cde3e
Marina <marina8ww1@gmail.com>
2023-11-28 13:43:29 +0700

    28,11 Вылетает приложение

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET(
+        "services/rest/? method=flickr.interestingness.getList" +
+                "&api_key=в8c2e712a8521d1fa2ff1dbfdd64099b5" + "&format=json" +
+                "&nojsoncallback=1" + "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+
+}
